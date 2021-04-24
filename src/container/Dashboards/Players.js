5bd61903d7a71a39576e21ed428c6437c9c8a81b
nipun3120 <nipunverma39@gmail.com>
2021-04-25 04:36:40 +0530

    .

--- src/container/Dashboards/Players.js
+++ src/container/Dashboards/Players.js
@@ -77,7 +77,7 @@ const Players = ({ players }) => {
                 </TableCell>
                 <TableCell>
                   {" "}
-                  <Typography color="primary">Base Price </Typography>{" "}
+                  <Typography color="primary">Base Price</Typography> {" "}
                 </TableCell>
               </TableRow>
             </TableHead>
